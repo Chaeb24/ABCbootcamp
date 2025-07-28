d5d380ef48712237d1fc96e3d297e8757923a57b
Chaebin Park <jisusushi@gmail.com>
2025-07-28 14:23:02 +0900

    streamlit 버전의 관상보기 구현

---
+++ 04-webapp.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from dotenv import load_dotenv
+from ai import get_personality_analysis
+
+load_dotenv()
+
+st.title("AI 관상 보기 프로그램")
+st.write("-----")
+
+st.write("안녕하세요! AI 관상가입니다")
+
+face_desc = st.text_area("얼굴 특징을 입력하세요")
+face_desc = face_desc.strip()
+
+if st.button("관상을 봐주세요",type="primary"):
+    if face_desc:
+        with st.spinner("관상을 분석 중입니다..."):
+            result = get_personality_analysis(face_desc)
+            st.write("----") # markdown, 구분선
+            st.write("관상 분석이 끝났습니다.")
+            st.info(result)
+    else:
+        st.error("얼굴 특징을 입력하고 관상보기 버튼을 클릭해주세요.")
